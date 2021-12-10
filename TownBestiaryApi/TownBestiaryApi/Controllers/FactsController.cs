54e989ec3e1d2b0255dfdd16efcf81dbd88ea810
Morasiu <moras2601@wp.pl>
2021-12-11 00:53:33 +0100

    Facts

---
+++ TownBestiaryApi/TownBestiaryApi/Controllers/FactsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TownBestiaryApi.Extensions;
+using TownBestiaryApi.Models.Facts;
+
+namespace TownBestiaryApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FactsController : ControllerBase {
+	[HttpGet]
+	public ActionResult<Fact> Get() {
+		return Ok(FactStorage.Facts.Random());
+	}
+}
---
+++ TownBestiaryApi/TownBestiaryApi/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace TownBestiaryApi.Extensions; 
+
+public static class IEnumerableExtensions {
+	public static T Random<T>(this IEnumerable<T> enumerable) {
+		var random = new Random();
+		var e = enumerable as T[] ?? enumerable.ToArray();
+		var count = e.Count();
+		var index = random.Next(0, count);
+		return e.ElementAt(index);
+	}
+}
