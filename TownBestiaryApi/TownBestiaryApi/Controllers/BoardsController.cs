a8fc3d5441832e3de933d914d467378a851fcedb
Morasiu <moras2601@wp.pl>
2021-12-11 03:36:24 +0100

    Collect logic

--- TownBestiaryApi/TownBestiaryApi/Controllers/BoardsController.cs
+++ TownBestiaryApi/TownBestiaryApi/Controllers/BoardsController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TownBestiaryApi.Models.Boards;
+using TownBestiaryApi.Models.Users;
 
 namespace TownBestiaryApi.Controllers; 
 [ApiController]
@@ -8,6 +10,9 @@ public class BoardsController : ControllerBase {
 
     [HttpPost("{id}/collect")]
     public ActionResult Post([FromRoute] int id) {
+        var board = BoardsStorage.Boards.SingleOrDefault(a => a.Id == id);
+        if (board is null) return NotFound();
+        UserStorage.User.CollectedBoards.Add(board);
         return Ok();
     }
 }
