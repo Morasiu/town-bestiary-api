22449126e083295ff924c0879ef26f6e10af8445
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-12-11 01:39:18 +0100

    User

---
+++ TownBestiaryApi/TownBestiaryApi/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TownBestiaryApi.Models;
+using TownBestiaryApi.Models.Reports;
+
+namespace TownBestiaryApi.Controllers; 
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase {
+    [HttpGet("{id}")]
+    public ActionResult<User> GetById([FromRoute] int id) {
+        var user = new User {
+            Id = 1,
+            UserName = "CrazyHedgehog77",
+            Reports = new List<Report> {
+                new Report {
+                    Id = 1,
+                    Date = DateTime.Now.AddDays(-7),
+                    Type = ReportType.InjuredAnimal,
+                    Localization = "Zagórze 9",
+                    Message = "Ranna sarna leży w rowie przy drodze"
+                },
+                new Report {
+                    Id = 2,
+                    Date = DateTime.Now.AddDays(-20),
+                    Type = ReportType.DangerousAnimal,
+                    Localization = "Liściasta 8",
+                    Message = "Stado dzików przy śmietniku"
+                }
+            },
+            // TODO
+            // CollectedBoards = 
+        };
+        return user;
+    }
+}
---
+++ TownBestiaryApi/TownBestiaryApi/Models/Reports/Report.cs
@@ -0,0 +1,9 @@
+﻿namespace TownBestiaryApi.Models.Reports; 
+
+public class Report {
+    public int Id { get; set; }
+    public string Message { get; set; }
+    public ReportType Type { get; set; }
+    public DateTime Date { get; set; }
+    public string Localization { get; set; }
+}
