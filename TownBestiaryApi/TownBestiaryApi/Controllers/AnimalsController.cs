75aac01fae71d1a2e06a7a36ce6ccaf5eb6fc66b
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-12-11 02:32:25 +0100

    animal adoption

---
+++ TownBestiaryApi/TownBestiaryApi/Controllers/AnimalsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TownBestiaryApi.Controllers; 
+
+[ApiController]
+[Route("[controller]")]
+public class AnimalsController : ControllerBase {
+    [HttpPost("adoption")]
+    public ActionResult PostAdoption() {
+        return Ok();
+    }
+}
