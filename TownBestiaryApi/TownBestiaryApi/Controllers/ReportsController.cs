94033ab089ca65dcccadb5599094d26c331741c0
Morasiu <moras2601@wp.pl>
2021-12-11 03:19:03 +0100

    Reports

--- TownBestiaryApi/TownBestiaryApi/Controllers/ReportsController.cs
+++ TownBestiaryApi/TownBestiaryApi/Controllers/ReportsController.cs
@@ -6,11 +6,10 @@ namespace TownBestiaryApi.Controllers;
 [ApiController]
 [Route("[controller]")]
 public class ReportsController : ControllerBase {
-    private List<Report> _reports = new List<Report>();
 
     [HttpPost]
     public ActionResult Post(Report report) {
-        _reports.Add(report);
+        ReportStorage.Reports.Add(report);
         return Ok();
     }
 }
---
+++ TownBestiaryApi/TownBestiaryApi/Models/Reports/ReportStorage.cs
@@ -0,0 +1,13 @@
+﻿namespace TownBestiaryApi.Models.Reports; 
+
+public class ReportStorage {
+	public static List<Report> Reports { get; set; } = new() {
+		new Report {
+			Date = DateTime.Now.AddDays(-1),
+			Type = ReportType.DangerousAnimal,
+			Message = "Dzik!",
+			Longitude = 50.0094m,
+			Latitude = 19.9212m,
+		}
+	};
+}
