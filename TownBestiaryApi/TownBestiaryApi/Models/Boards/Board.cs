0048bdcca675e559964ea545a93fb0dab10451e6
Morasiu <moras2601@wp.pl>
2021-12-11 03:41:04 +0100

    Species

--- TownBestiaryApi/TownBestiaryApi/Models/Boards/Board.cs
+++ TownBestiaryApi/TownBestiaryApi/Models/Boards/Board.cs
@@ -2,7 +2,7 @@
 
 public class Board {
 	public int Id { get; set; }
-	public int AnimalId { get; set; }
+	public int SpeciesId { get; set; }
 	public string Name { get; set; }
 	public string Description { get; set; }
 	public string Localization { get; set; }
